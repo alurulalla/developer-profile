4d9e0d59168293d903b6edde63d9d687a3821c07
Aluru Murali Krishna <alurumuralikrishna9@gmail.com>
2021-03-02 15:45:25 +0530

    Added node path

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ console.log(process.env.NODE_ENV);
 app.use(express.static(path.join(__dirname, 'public')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(__dirname, '/client/build'));
+  app.use(express.static(path.join(__dirname, '/client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
